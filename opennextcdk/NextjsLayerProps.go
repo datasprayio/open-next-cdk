13138f972d89deb5f7f4e7198f27d1b2349e4203
Matus Faro <matus@matus.io>
2023-07-17 14:05:17 -0400

    Remove Go release from main

--- opennextcdk/NextjsLayerProps.go
+++
@@ -1,7 +0,0 @@
-// Deploy a NextJS app using OpenNext packaging to serverless AWS using CDK
-package opennextcdk
-
-
-type NextjsLayerProps struct {
-}
-
--- opennextcdk/internal/types.go
+++
@@ -1,8 +0,0 @@
-package internal
-import (
-	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
-	"github.com/aws/constructs-go/constructs/v10"
-)
-type Type__awslambdaFunction = awslambda.Function
-type Type__constructsConstruct = constructs.Construct
-type Type__awslambdaLayerVersion = awslambda.LayerVersion
